21ca7188cf86187b29aaddec0b89ce99b0d728f1
Ezekiel Emmanuel <ezekielemmanuel10@gmail.com>
2023-08-31 03:00:12 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - Compare two strings.
+ * wildcmp - compare two strings.
  * @s1: string 1.
  * @s2: string 2. It can contains a * as a special character.
  * Return: 1 if are identical, 0 if not.
